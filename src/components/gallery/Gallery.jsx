db2b7e0d0e7ac298d732e48a4ef33040cdc5bd76
Eva Fireborn <eva.fireborn@gmail.com>
2020-01-29 13:50:35 +0100

    fixes issue in footer and connects all pages with SQL

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import API from '../../helperFunctions/ApiCalls';
 import './gallery.scss';
 
 const Gallery= () => {
+    const [content, setContent] = useState(null);
+
+    useEffect(()=> {
+        API.getGalleryPage(res => {
+            console.log('res: ', res);
+            if (res[0].type === 'pageContent') {
+                setContent(res[0].content);
+            }
+        });
+    }, []);
+
+    const createMarkup = () => {
+        return {__html: content}
+    }
     return (
-        <div>
-            <h3>Galleryt</h3>
+        <div className="info">
+            {content ? (
+                <div className="wrapper" dangerouslySetInnerHTML={createMarkup()}>
+
+                </div>
+            ) : (<p>Laddar informationen</p>)}
         </div>
     )
 }
--- src/components/workshops/Workshops.jsx
+++ src/components/workshops/Workshops.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import API from '../../helperFunctions/ApiCalls';
 import './workshops.scss';
 
 const Workshops= () => {
+    const [content, setContent] = useState(null);
+
+    useEffect(()=> {
+        API.getWorkshopPage(res => {
+            console.log('res: ', res);
+            if (res[0].type === 'pageContent') {
+                setContent(res[0].content);
+            }
+        });
+    }, []);
+
+    const createMarkup = () => {
+        return {__html: content}
+    }
     return (
-        <div>
-            <h3>Workshops</h3>
+        <div className="info">
+            {content ? (
+                <div className="wrapper" dangerouslySetInnerHTML={createMarkup()}>
+
+                </div>
+            ) : (<p>Laddar informationen</p>)}
         </div>
     )
 }
